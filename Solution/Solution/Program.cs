69c022e1347a7c108c5e85a851268a30aea22927
nisslyak <nissopodolyak@gmail.com>
2024-06-03 10:23:06 +0300

    First commit

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int lonelyinteger(List<int> a)
+    {
+        var g = a.GroupBy(i => i);
+
+        int lonelyInteger = 0;
+
+        return a.GroupBy(i => i)
+            .Where(grp => grp.Count() == 1)
+            .Select(grp => grp.Key)
+            .FirstOrDefault();
+ 
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.lonelyinteger(a);
+    }
+}
